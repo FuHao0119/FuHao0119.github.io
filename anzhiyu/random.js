49b1f18affa1db5e39f303dd3d3b327a54d4ee3e
cumin1 <3144941746@qq.com>
2025-09-11 21:56:35 +0800

    Site updated: 2025-09-11 21:56:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/10/常用数据结构/","2025/09/10/数据结构导论笔记/"];function toRandomPost(){
+var posts=["2025/09/10/数据结构导论笔记/","2025/09/10/常用数据结构/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
