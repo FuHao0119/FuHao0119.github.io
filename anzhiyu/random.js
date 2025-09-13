9d2fb32102caef9ba244ffd527c1dafe77d6f112
cumin1 <3144941746@qq.com>
2025-09-13 11:16:45 +0800

    Site updated: 2025-09-13 11:16:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/10/数据结构导论笔记/","2025/09/10/常用数据结构/"];function toRandomPost(){
+var posts=["2025/09/10/数据结构导论笔记/","2025/09/10/常用数据结构/","2025/09/11/解决hyprland环境下pacman-Syu-后-gestures-workspace-swipe配置问题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
